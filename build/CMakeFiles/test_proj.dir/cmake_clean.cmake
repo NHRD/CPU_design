f8b63b4d0b8dd47656c4381f46dddf7c787944af
NHRD <naohisa.harada@gmail.com>
2020-07-24 23:46:55 +0900

    aa

---
+++ build/CMakeFiles/test_proj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_proj.dir/main.cpp.o"
+  "test_proj"
+  "test_proj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/naohisa/Desktop/CPU_design
+# Build directory: /home/naohisa/Desktop/CPU_design/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
