0746d28485d8a41ea81e5aec9a6d5b36043185b8
NHRD <naohisa.harada@gmail.com>
2020-08-02 19:00:00 +0900

    a

---
+++ FPGA/dec_7seg/dec_7seg_hdl.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity adder_4bit is
+    port(
+        BIN :   in std_logic_vector(3 downto 0);
+        SUM :   out std_logic_vector(6 downto 0)
+    );
+end adder_4bit;
+
+architecture RTL of dec_7seg_hdl is
+
+    signal not_BIN(0)   :   std_logic_vector
+
+begin
+    
+
+end RTL;
+
