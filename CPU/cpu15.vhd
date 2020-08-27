56461c91ede3e2f37e894ddcea3cc6503134bb3f
NHRD <naohisa.harada@gmail.com>
2020-08-27 13:43:20 +0900

    a

--- CPU/cpu15.vhd
+++ CPU/cpu15.vhd
@@ -53,5 +53,50 @@ component reg_dc
 	(
 		CLK_DC		:	in std_logic;
 		N_REG_IN	:	in std_logic_vector(2 downto 0);
+		REG_0		:	in std_logic_vector(15 downto 0);
+		REG_1		:	in std_logic_vector(15 downto 0);
+		REG_2		:	in std_logic_vector(15 downto 0);
+		REG_3		:	in std_logic_vector(15 downto 0);
+		REG_4		:	in std_logic_vector(15 downto 0);
+		REG_5		:	in std_logic_vector(15 downto 0);
+		REG_6		:	in std_logic_vector(15 downto 0);
+		REG_7		:	in std_logic_vector(15 downto 0);
+		N_REG_OUT	:	out std_logic_vector(2 downto 0);
+		REG_OUT		:	out std_logic_vector(15 downto 0)
+	);
+end component;
+
+--ram_dc
+component ram_dc
+	port
+	(
+		CLK_DC		:	in std_logic;
+		RAM_AD_IN	:	in std_logic_vector(7 downto 0);
+		RAM_0		:	in std_logic_vector(15 downto 0);
+		RAM_1		:	in std_logic_vector(15 downto 0);
+		RAM_2		:	in std_logic_vector(15 downto 0);
+		RAM_3		:	in std_logic_vector(15 downto 0);
+		RAM_4		:	in std_logic_vector(15 downto 0);
+		RAM_5		:	in std_logic_vector(15 downto 0);
+		RAM_6		:	in std_logic_vector(15 downto 0);
+		RAM_7		:	in std_logic_vector(15 downto 0);
+		IO65_IN		:	in std_logic_vector(15 downto 0);
+		RAM_AD_OUT	:	out std_logic_vector(7 downto 0);
+		RAM_OUT		:	out std_logic_vector(15 downto 0)
+	);
+end component;
+
+--exec
+component exec
+	port
+	(
+		CLK_EX		:	in std_logic;
+		RESET_N		:	in std_logic;
+		OP_CODE		:	in std_logic_vector(3 downto 0);
+		REG_A		:	in std_logic_vector(15 downto 0);
+		REG_B		:	in std_logic_vector(15 downto 0);
+		OP_DATA		:	in std_logic_vector(7 downto 0);
+		RAM_OUT		:	in std_logic_vector(15 downto 0);
+		P_COUNT		:	out std_logic_vector(7 downto 0);
 		
 	)
